94361ca6a85567d1770b2a2b11b56ff6ad26fed9
Gosttlt <55979136+Gosttlt@users.noreply.github.com>
2021-08-03 21:25:47 +0400

    Update Line.js

--- src/components/Line.js
+++ src/components/Line.js
@@ -3,6 +3,11 @@ import styled from "styled-components"
 const StyledLine = styled.div`
 font-size: 24px;
 color:  ${props => props.color || props.theme.colors.primory}
+${props => props.outlined && css`
+    color: ${props => props.color || 'white'};
+    border: 1px solid ${props => props.color || "white"};
+    background: transparent;
+`}
 `
 
 export const Line = (props) => {
